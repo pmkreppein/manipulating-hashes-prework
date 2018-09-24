f6a32434654d72e861c30bfd1ce134ff68f28ddf
Peter Kreppein <pmkreppein@gmail.com>
2018-09-24 00:58:30 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -13,8 +13,8 @@ def first_challenge
     }
   }
 
-  contacts["Freddy Mercury"].each do |attribute, value|
-      if attribute == :favorite_icecream_flavors
+  contacts["Freddy Mercury"].each do |key, value|
+      if key == :favorite_icecream_flavors
         value.delete_if do |flavor|
           flavor == "strawberry"
       end
